43a3fc45ccc02c47631e3e6d7a07de44c96a0e6d
eli <ebezhiasvhili@gmail.com>
2024-11-03 20:12:11 +0400

    add student and teacher model

--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 class User(AbstractUser):
     class Role(models.TextChoices):
@@ -16,3 +17,40 @@ class User(AbstractUser):
             self.role = self.base_role
             return super().save(*args, **kwargs)
 # Create your models here.
+
+class StudentManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.STUDENT)
+
+
+class Student(User):
+
+    base_role = User.Role.STUDENT
+
+    student = StudentManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "Only for Students"
+
+
+class TeacherManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.TEACHER)
+
+
+class Teacher(User):
+
+    base_role = User.Role.TEACHER
+
+    teacher = TeacherManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "Only for Teachers "
