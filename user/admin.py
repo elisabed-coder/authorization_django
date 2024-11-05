8853b346402ed8a662b5c9b82b798818e3089549
eli <ebezhiasvhili@gmail.com>
2024-11-05 20:04:11 +0400

    add models in admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import User, Student, Teacher
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('role',)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'subject')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('subject',)
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'subject')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('subject',)
+
+# Register your models
+admin.site.register(User, UserAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Teacher, TeacherAdmin)
